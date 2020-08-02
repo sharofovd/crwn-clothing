0f3b064272a461209c7a09fcd58bcee00f51b89b
Your Name <you@exampl>
2020-08-02 23:17:56 +0500

    integrated firebase into application, created sign in page with sign in componentsign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -50,7 +51,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
+          <div className='buttons'>
           <CustomButton type='submit'> Sign in </CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+          {' '}
+          Sign in width Google{' '}</CustomButton>
+          </div>
         </form>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,24 +3,47 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBjbPtRAiYRaKkZ3tF9Y83NDerb3NpY65Q",
-    authDomain: "crwn-db-84424.firebaseapp.com",
-    databaseURL: "https://crwn-db-84424.firebaseio.com",
-    projectId: "crwn-db-84424",
-    storageBucket: "crwn-db-84424.appspot.com",
-    messagingSenderId: "1023373578155",
-    appId: "1:1023373578155:web:146570fae91c28696a0cee",
-    measurementId: "G-FX0KVDGTRB"
-  };
+  apiKey: "AIzaSyBjbPtRAiYRaKkZ3tF9Y83NDerb3NpY65Q",
+  authDomain: "crwn-db-84424.firebaseapp.com",
+  databaseURL: "https://crwn-db-84424.firebaseio.com",
+  projectId: "crwn-db-84424",
+  storageBucket: "crwn-db-84424.appspot.com",
+  messagingSenderId: "1023373578155",
+  appId: "1:1023373578155:web:146570fae91c28696a0cee",
+  measurementId: "G-FX0KVDGTRB"
+};
+firebase.initializeApp(config);
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
+  const snapShot = await userRef.get();
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
 
-  export default firebase;
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
